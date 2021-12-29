f78e450ea4242e7f068903eb60d6845d416d0811
StuFF mc <m+git@carrascomolina.com>
2021-12-29 14:36:38 +0100

    Initial Commit

---
+++ Alby/AlbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbyApp.swift
+//  Alby
+//
+//  Created by StuFF mc on 29.12.21.
+//
+
+import SwiftUI
+
+@main
+struct AlbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alby/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alby
+//
+//  Created by StuFF mc on 29.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
