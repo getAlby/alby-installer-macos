86a67bad445ca491006492e42bc73ffd63ef16ab
StuFF mc <m+git@carrascomolina.com>
2022-01-05 23:20:49 +0100

    Code now lists installed browsers, and allows install of JSON + start of extension install

--- App.swift
+++ App.swift
@@ -3,89 +3,160 @@ import SwiftUI
 
 @main
 struct Window: App {
-    private var alby = Alby()
     @State var localizedError: String?
     @State var loaded = false
+    @State var browsers: [Browser] = []
 
     var body: some Scene {
         WindowGroup {
-            Group {
+            HStack(alignment: .bottom) {
                 if loaded {
-                    Button("Install JSON") {
-                        do {
-                            try alby.install()
-                        } catch {
-                            localizedError = error.localizedDescription
-                        }
+                    VStack(alignment: .leading) {
+                        EmptyView()
+                            .frame(width: 128, height: 128)
+                        Label("Install the extension", systemImage: "1.circle")
+                        Label("Install the companion", systemImage: "2.circle")
                     }
+                    .padding(.leading)
+                    .font(.title)
+                    ForEach(browsers) {
+                        view(for: $0)
+                    }
+                    Spacer()
                 } else {
                     Text("Seeking Browsers...")
+                        .font(.largeTitle)
                 }
             }
             .alert(isPresented: .constant(localizedError?.isEmpty == false)) {
                 Alert(title: Text(localizedError!))
             }
-            .frame(width: 640, height: 480)
+            .frame(width: CGFloat((browsers.count * 200) + 200), height: 240)
             .onAppear {
-                loaded = alby.seek()
+                browsers = Browser.installed
+                loaded = true
+            }
+        }
+    }
+
+    func view(for browser: Browser) -> some View {
+        VStack {
+            if let icon = browser.icon {
+                ZStack {
+                    Image(nsImage: icon)
+                        .resizable()
+                        .frame(width: 128, height: 128)
+                    if browser.companionInstalled {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                            .font(.largeTitle.bold())
+                    }
+                }
+            }
+            Button("Install") { // Extension (Also green check mark?!)
+                do {
+                    if let url = URL(string: "https://getalby.com/install/")?.appendingPathComponent(browser.id),
+                    let application = browser.application {
+                        NSWorkspace.shared.open([url], withApplicationAt: application, configuration: NSWorkspace.OpenConfiguration())
+                        try browser.installOrRemove()
+                    }
+                } catch {
+                    localizedError = error.localizedDescription
+                }
+            }
+            Button {
+                do {
+                    try browser.installOrRemove()
+                } catch {
+                    localizedError = error.localizedDescription
+                }
+            } label: {
+                if browser.companionInstalled {
+                    Text("Remove")
+                } else {
+                    Text("Install")
+                }
             }
         }
     }
 }
 
-struct Alby {
-    private let fm = FileManager.default
+struct Browser: Identifiable {
+    let id: String
+    let appSupportFolder: String
+    let chrome: Bool
 
-    private let json = """
-    {
-    "name": "alby",
-    "description": "Alby native messaging to connect to nodes behind Tor",
-    "path": "./target/release/alby",
-    "type": "stdio",
-    "allowed_extensions": [ "extension@getalby.com" ]
+    var json: String? {
+        guard let bundle = Bundle.main.resourcePath else {
+            return nil
+        }
+        return """
+            {
+            "name": "alby",
+            "description": "Alby native messaging to connect to nodes behind Tor",
+            "path": "\(bundle)/alby",
+            "type": "stdio",
+            "allowed_\(chrome ? "origins" : "extensions")": [ "extension@getalby.com" ]
+            }
+            """
     }
-    """
 
-    var nativeMessagingURL: URL {
-        appSupportURL.appendingPathComponent("Mozilla/NativeMessagingHosts")
+    static var all: [Browser] {
+        [Browser(id: "org.mozilla.firefox", appSupportFolder: "Mozilla", chrome: false),
+         Browser(id: "com.google.Chrome", appSupportFolder: "Google/Chrome", chrome: true),
+         Browser(id: "org.chromium.Chromium", appSupportFolder: "Chromium", chrome: true)]
+    }
+
+    static var installed: [Browser] {
+        all.filter { $0.path != nil }
+    }
+
+    var path: URL? {
+        NSWorkspace.shared.urlForApplication(withBundleIdentifier: id)
+    }
+
+    private var nativeMessagingURL: URL {
+        let appSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+        let url = appSupportURL.appendingPathComponent(appSupportFolder).appendingPathComponent("NativeMessagingHosts")
+        try? FileManager.default.createDirectory(at: url, withIntermediateDirectories: false, attributes: nil)
+        return url
     }
 
     var albyJsonURL: URL {
         nativeMessagingURL.appendingPathComponent("alby").appendingPathExtension("json")
     }
 
-    private var appSupportURL: URL {
-        fm.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+    var companionInstalled: Bool {
+        FileManager.default.fileExists(atPath: albyJsonURL.path)
     }
 
-    func seek() -> Bool {
-        fm.fileExists(atPath: nativeMessagingURL.path)
+    var exists: Bool {
+        FileManager.default.fileExists(atPath: nativeMessagingURL.path)
     }
 
-    func install() throws {
-        try json.write(to: albyJsonURL, atomically: true, encoding: .ascii)
+    func installOrRemove() throws {
+        if companionInstalled {
+            try FileManager.default.removeItem(at: albyJsonURL)
+        } else {
+            try json?.write(to: albyJsonURL, atomically: true, encoding: .ascii)
+        }
+    }
+
+    var application: URL? {
+        NSWorkspace.shared.urlForApplication(withBundleIdentifier: id)
+    }
+
+    var icon: NSImage? {
+        if let url = application, let bundle = Bundle(url: url)?.resourcePath,
+           let files = try? FileManager.default.contentsOfDirectory(atPath: bundle) {
+            for file in files {
+                if file.hasSuffix("icns") {
+                    if let image = Bundle(url: url)?.resourceURL?.appendingPathComponent(file) {
+                        return NSImage(contentsOf: image)
+                    }
+                }
+            }
+        }
+        return nil
     }
 }
-
-// TODO: List
-// - Get the App Icons from the system or Bundle them (easier)?
-// - Where do we copy the Extension itself? alby-1.1.0-fx.zip? Is is smart to build it, or should we rather download it (smarter) and put a copy then in the right folder.
-//     --> I need the URL where I can download it. https://addons.mozilla.org/firefox/downloads/file/3887973/alby_bitcoin_lightning_wallet-1.2.1-fx.xpi ? Is this URL fixed?
-// - List @
-
-//
-//private getDarwinNMHS() {
-//    return {
-//      Firefox: `${this.homedir()}/Library/Application\ Support/Mozilla/`,
-//      Chrome: `${this.homedir()}/Library/Application\ Support/Google/Chrome/`,
-//      "Chrome Beta": `${this.homedir()}/Library/Application\ Support/Google/Chrome\ Beta/`,
-//      "Chrome Dev": `${this.homedir()}/Library/Application\ Support/Google/Chrome\ Dev/`,
-//      "Chrome Canary": `${this.homedir()}/Library/Application\ Support/Google/Chrome\ Canary/`,
-//      Chromium: `${this.homedir()}/Library/Application\ Support/Chromium/`,
-//      "Microsoft Edge": `${this.homedir()}/Library/Application\ Support/Microsoft\ Edge/`,
-//      "Microsoft Edge Beta": `${this.homedir()}/Library/Application\ Support/Microsoft\ Edge\ Beta/`,
-//      "Microsoft Edge Dev": `${this.homedir()}/Library/Application\ Support/Microsoft\ Edge\ Dev/`,
-//      "Microsoft Edge Canary": `${this.homedir()}/Library/Application\ Support/Microsoft\ Edge\ Canary/`,
-//      Vivaldi: `${this.homedir()}/Library/Application\ Support/Vivaldi/`,
-//    };
-//  }
