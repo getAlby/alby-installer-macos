1095d950af3ea0547166a37aba43b14133237e9b
Alex Vihlayew <vihlayew.alex@gmail.com>
2022-01-10 16:15:17 +0200

    Add DMG creation script

---
+++ DMG/create-dmg.sh
@@ -0,0 +1,13 @@
+brew install create-dmg
+rm -rf Alby-Installer.dmg
+create-dmg \
+  --volname "Alby Installer" \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "Alby.app" 200 190 \
+  --hide-extension "Alby.app" \
+  --app-drop-link 600 185 \
+  --no-internet-enable \
+  --hdiutil-quiet \
+  "Alby-Installer.dmg" \
+  "dmg_content/"
